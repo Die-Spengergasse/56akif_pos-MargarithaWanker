1720765e673a4b28821d6310b0d126bf396ccdaa
MargarithaWanker <WAN210096@spengergasse.at>
2022-10-06 15:46:42 +0200

    ExProperties erledigt

---
+++ ExProperties/ExProperties/Lehrer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExProperties
+{
+    public class Lehrer
+    {
+        private string  vorname, zuname;
+        private decimal? bruttogehalt;
+
+        public string Vorname { get; set; } = "";
+        public string? Zuname { get; set; }
+
+        public decimal? Bruttogehalt
+        {
+            get { return bruttogehalt; }
+            set { bruttogehalt ??= value; }
+        }
+
+        public string? Kuerzel
+        {
+            get { return Zuname?.ToUpper().Substring(0, 3) ?? ""; }
+        }
+
+        public decimal Nettogehalt
+        {
+            get { return (Bruttogehalt * 0.8M) ?? 0M; }
+        }
+    }
+}
---
+++ ExProperties/ExProperties/Rechteck.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExProperties
+{
+    public class Rechteck
+    {
+        private int laenge, breite;
+        
+
+        public int Laenge
+        {
+            get { return laenge; }
+            set { laenge = value >= 0 ? value : throw new ArgumentException("Ungültige Laenge!"); }
+        }
+        public int Breite
+        {
+            get { return breite; }
+            set { breite = value >= 0 ? value : throw new ArgumentException("Ungültige Breite!"); }
+        }
+
+        public int Flaeche
+        {
+            get { return Laenge * Breite; }
+        }
+
+    }
+}
